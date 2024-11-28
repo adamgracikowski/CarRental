5cf996184d8e84ad67b86feef8b5d050e4741f0b
Antonina Frąckowiak <01180776@pw.edu.pl>
2024-11-28 07:28:11 +0000

    Merged PR 37: Dodanie walidacji przy generowaniu ofert i formularza dla użytkowników bez uzupełnionych danych
    ![Zrzut ekranu 2024-11-22 151621.png](https://dev.azure.com/PW-Sem05/350ffeda-5390-4a1e-a4fe-3249bc8a3b1e/_apis/git/repositories/2158ce63-61be-49d0-a903-f9714d610de9/pullRequests/37/attachments/Zrzut%20ekranu%202024-11-22%20151621.png) ![Zrzut ekranu 2024-11-22 151748.png](https://dev.azure.com/PW-Sem05/350ffeda-5390-4a1e-a4fe-3249bc8a3b1e/_apis/git/repositories/2158ce63-61be-49d0-a903-f9714d610de9/pullRequests/37/attachments/Zrzut%20ekranu%202024-11-22%20151748.png)
    
    Related work items: #40, #110

--- CarRental/CarRental.Comparer.Web/Requests/DTOs/Offers/CreateOfferDto.cs
+++ CarRental/CarRental.Comparer.Web/Requests/DTOs/Offers/CreateOfferDto.cs
@@ -1,10 +1,13 @@
-﻿namespace CarRental.Comparer.Web.Requests.DTOs.Offers;
+﻿using CarRental.Comparer.Web.Requests.ValidationConstants;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Comparer.Web.Requests.DTOs.Offers;
 
 public sealed class CreateOfferDto
 {
 	public CreateOfferDto()
 	{
-		
+
 	}
 	public CreateOfferDto(int drivingLicenseYears, int age, decimal latitude, decimal longitude)
 	{
@@ -14,8 +17,15 @@ public sealed class CreateOfferDto
 		Longitude = longitude;
 	}
 
+	[Range(UserDtoValidationConstants.DrivingLicenseYearsConstants.LowerYears,
+	CreateOfferDtoValidationConstants.DrivingLicenseYearsConstants.UpperYears,
+	ErrorMessage = CreateOfferDtoValidationConstants.DrivingLicenseYearsConstants.DrivingLicenseYearsRange)]
 	public int DrivingLicenseYears { get; set; }
-    public int Age { get; set; }
-    public decimal Latitude { get; set; }
-    public decimal Longitude { get; set; }
+
+	[Range(CreateOfferDtoValidationConstants.AgeConstants.LowerAge,
+	CreateOfferDtoValidationConstants.AgeConstants.UpperAge,
+	ErrorMessage = CreateOfferDtoValidationConstants.AgeConstants.AgeRange)]
+	public int Age { get; set; }
+	public decimal Latitude { get; set; }
+	public decimal Longitude { get; set; }
 }
---
+++ CarRental/CarRental.Comparer.Web/Requests/ValidationConstants/CreateOfferDtoValidationConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace CarRental.Comparer.Web.Requests.ValidationConstants;
+
+public static class CreateOfferDtoValidationConstants
+{
+	public static class AgeConstants
+	{
+		public const int LowerAge = 18;
+		public const int UpperAge = 100;
+		public const string AgeRange = "Age must be between 18 and 100.";
+	}
+	public static class DrivingLicenseYearsConstants
+	{
+		public const int LowerYears = 1;
+		public const int UpperYears = 100;
+		public const string DrivingLicenseYearsRange = "Driving License Years must be between 1 and 100.";
+	}
+
+}
